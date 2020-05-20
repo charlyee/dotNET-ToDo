563c972e8a94d9dfe3d39ebd2514d01ee8264064
Warren Uhrich <hello@warren.codes>
2020-05-20 11:25:00 -0600

    Update startup with database context.

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ToDo.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace ToDo
 {
@@ -24,6 +26,9 @@ namespace ToDo
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+
+            services.AddDbContext<ToDoContext>( options => 
+                options.UseSqlServer( Configuration.GetConnectionString( "ToDoContext" ) ) );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
